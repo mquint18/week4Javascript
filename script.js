e975ee86b5200511e5c7fbdb3a14159dbbe0c388
Michael <michaelquint@gmail.com>
2026-05-06 19:42:40 -0400

    some javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello javascript");
+console.log("What is this?");
+console.log("Michael Quint");
+
+alert("Welcome to Javascript");
+
+
+//variables
+
+let name = "Michael";
+let age = 46;
+
+console.log(name);
+console.log(age);
+
+
+let title = "Everbody Poops";
+let author = "IP Daily";
+let year = 1998;
+let pageCount = 43;
+
+console.log(title);
+console.log(author);
+console.log(year)
+console.log(pageCount)
